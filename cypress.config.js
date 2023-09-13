2f81b2f6217a42329015c474498aa6fb79c79a82
A-202332 <v-salagarsamy@expediagroup.com>
2023-09-13 08:26:48 +0530

    bs added

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    reportFilename: '[status]_[datetime]-report',
+    reportPageTitle: 'Automation Report',
+    embeddedScreenshots: true,
+    overwrite: false,
+    html: true,
+    saveJson: true,
+    charts: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
